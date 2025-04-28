04bca1f65db03d567c16d5d6d1b54a67524817d8
ThomasPrins <thomas.prins1@hotmail.com>
2025-04-28 13:05:03 +0200

    Initial push

---
+++ Assignment.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 16 12:59:05 2025
+
+@author: user
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# some sensor network 100m x 100m
+# randomly distributed sensors (geometric?)
+# blabla
